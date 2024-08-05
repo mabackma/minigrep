e3c56d8f33e1687e8b3fd8ece298879e54e89131
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-08-06 02:50:25 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::process;
+use minigrep::{Config, run};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+/*
+    let result = Config::build(&args);
+
+    let config = match result {
+        Ok(config) => config,
+        Err(e) => {
+            println!("Problem parsing arguments: {}", e);
+            return;
+        }
+    };
+*/
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+ 
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
